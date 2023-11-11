4d1f0ef2921a85317f928afb2966b3b92453cac9
akkgit <107975491+akkgit@users.noreply.github.com>
2023-11-10 22:02:05 -0600

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,10 @@
+// This is a single-line comment
+
+/*
+  This is a multiline comment
+  that spans multiple lines.
+  It is often used for explanations
+  or temporarily disabling code.
+*/
+
+console.log("Hello, World!"); // This is another single-line comment
